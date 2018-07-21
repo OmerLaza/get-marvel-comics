f96164b568213caa325b6cd2ed327b1f0e7b6316
OMER\omer <a@a.com>
2018-07-21 13:25:47 +0300

    basic functionality, still need to write to multiple files

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
+from marvel.marvel import Marvel
+from keys import private_key, public_key
+import json
+
+base_file_name = 'comics{0}.json'
+
+basic_limit = 100
+starting_offset = 0
+number_to_bring = 200
+
+
+def del_file_content():
+    with open(base_file_name, 'w') as the_file:
+        the_file.write("")
+
+
 def main():
-    pass
+    m = Marvel(public_key, private_key)
+    del_file_content()
+    for i in xrange(starting_offset, number_to_bring, basic_limit):
+        comics = m.get_comics(limit=basic_limit, offset=i, )
+        with open(base_file_name.format(), 'a') as the_file:
+            the_file.write(json.dumps(comics.data.to_dict()['results']))
 
 
 if __name__ == '__main__':
